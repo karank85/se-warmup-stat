07a5bcb36a823455ea9f65bd0aa9a4da74bd672d
karank85 <kkjas85@gmail.com>
2023-01-13 15:30:10 +0700

    check case if no arg given

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import sys
 import numpy as np
 
-text_file = sys.argv[1]
+if len(sys.argv) == 2:
+    text_file = sys.argv[1]
+    try:
+        output = None
+        with open(text_file) as f:
+            output = f.readlines()
 
-try:
-    output = None
-    with open(text_file) as f:
-        output = f.readlines()
-
-    numbers = []
-    for n in output:
-        numbers += [int(n)]
+        numbers = []
+        for n in output:
+            numbers += [int(n)]
     
-    print("Statistic Summary")
-    print("mean:", np.mean(numbers))
-    print("std:",  np.std(numbers))
-    print("min:", np.min(numbers))
-    print("max:", np.max(numbers))
-except:
-    print("File not found")
+        print("Statistic Summary")
+        print("mean:", np.mean(numbers))
+        print("std:",  np.std(numbers))
+        print("min:", np.min(numbers))
+        print("max:", np.max(numbers))
+    except:
+        print("File not found")
+else:
+    print("Please give a file name")
+
+    
+
+
 
 
   
