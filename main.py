b30f0f32737cc600aafb32ea18f6968bd18d83d2
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-13 16:06:55 +0700

    Done

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import sys
 import numpy as np
+from typing import List
 
-if len(sys.argv) == 2:
-    text_file = sys.argv[1]
+def getResult(file_name, array_input: List[int], calculate_from_array: bool) -> List[int]:
     try:
         output = None
-        with open(text_file) as f:
-            output = f.readlines()
+        if (calculate_from_array == False):
+            with open(file_name) as f:
+                output = f.readlines()
+                f.close()
 
-        numbers = []
-        for n in output:
-            try:
-                numbers += [int(n)]
-            except:
-                print("Not number:", n)
+        numbers = array_input
+        if (calculate_from_array == False and output != None):
+            for n in output:
+                if (n.strip().isnumeric()):
+                    numbers += [int(n.strip())]
                 
-        if len(numbers) > 0:
-            print("Statistic Summary")
-            print("mean:", np.mean(numbers))
-            print("std:",  np.std(numbers))
-            print("min:", np.min(numbers))
-            print("max:", np.max(numbers))
-        else:
-            print("No numbers")
+        if calculate_from_array == False:
+            print(file_name)
+        print("mean:", np.mean(numbers), "std:", np.std(numbers), "min:", np.min(numbers), "max:", np.max(numbers))
+        return numbers
     except:
         print("File not found")
+        return []
+
+
+if len(sys.argv) > 1:
+    file_names = sys.argv
+    total = []
+    print("Statistic Summary")
+    for i in range(1, len(file_names)):
+        text_file = sys.argv[i]
+        result = getResult(text_file, [], False)
+        total += result
+        print("")
+    print("combined:")
+    getResult(None, total, True)
 else:
-    print("Please give a file name")
+    print("Please give file names")
 
     
 
